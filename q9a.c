4cf8cbd556eaaeaeedd89c540acde54547103388
Raj Singh <sraj08188@gmail.com>
2023-04-28 00:05:35 +0530

    Update q9a.c

--- q9a.c
+++ q9a.c
@@ -8,7 +8,7 @@ int main()
     for(f1=-1,f2=1;f<=n;f=f1+f2,f1=f2,f2=f)
     if(n==f)
      printf("The %d is a term of fibonacci series",n);
-    if(f>n)
+    else
      printf("The %d is not a term of fibonacci series",n);
     return 0;
 }
