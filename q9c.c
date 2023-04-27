1acaff375d81b0e146b65701e1c5c93cba3f1e87
Raj Singh <sraj08188@gmail.com>
2023-04-28 00:34:54 +0530

    Update q9c.c

--- q9c.c
+++ q9c.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-    
+    int number,k;
+    printf("Enter a Number :");
+    scanf("%d",&number);
+    k=sqrt(number);
+    if(k*k==number)
+    printf("%d is perfect square ",number);
+    else
+    printf("%d is perfect square ",number);
     return 0;
 }
