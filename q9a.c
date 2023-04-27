209c1071786b9beeb08acf4de925c2dd83b78502
Raj Singh <sraj08188@gmail.com>
2023-04-28 00:04:35 +0530

    Update q9a.c

--- q9a.c
+++ q9a.c
@@ -5,7 +5,7 @@ int main()
     int f,f1,f2,n;
     printf("Enter a number :");
     scanf("%d",&n);
-    for(f1=0,f2=1;f<=n;f=f1+f2,f1=f2,f2=f)
+    for(f1=-1,f2=1;f<=n;f=f1+f2,f1=f2,f2=f)
     if(n==f)
      printf("The %d is a term of fibonacci series",n);
     if(f>n)
